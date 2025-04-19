a53eddc8e7bfec3afdb11bcc144736503cd1a69a
Amir <amirsabaev50@gmail.com>
2025-04-19 14:14:08 +0300

    test4

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev_button.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/next_button.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/prev_button.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./icons/next_button.png"></button>',
       });
   });

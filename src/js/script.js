9c26a32727f0320db9f6aaeb01c748d346ae4090
Amir <amirsabaev50@gmail.com>
2025-04-19 13:21:20 +0300

    added slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev_button.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next_button.png"></button>',
+      });
+  });
